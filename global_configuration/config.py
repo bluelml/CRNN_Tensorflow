6ab109ec9cf29ccde5de91a61dd571a77d7532bd
bluelml <bluelml@gmail.com>
2018-10-26 11:00:35 +0800

    When training with huge dataset(my dataset > 300K)， big batch size and big lr decay step make loss easy converged

--- global_configuration/config.py
+++ global_configuration/config.py
@@ -30,7 +30,7 @@ __C.ARCH.INPUT_SIZE = (100, 32)
 __C.TRAIN = edict()
 
 # Set the shadownet training epochs
-__C.TRAIN.EPOCHS = 40000
+__C.TRAIN.EPOCHS = 80000
 # Set the display step
 __C.TRAIN.DISPLAY_STEP = 1
 # Set the test display step during training process
@@ -40,15 +40,15 @@ __C.TRAIN.MOMENTUM = 0.9
 # Set the initial learning rate
 __C.TRAIN.LEARNING_RATE = 0.1
 # Set the GPU resource used during training process
-__C.TRAIN.GPU_MEMORY_FRACTION = 0.85
+__C.TRAIN.GPU_MEMORY_FRACTION = 0.9
 # Set the GPU allow growth parameter during tensorflow training process
 __C.TRAIN.TF_ALLOW_GROWTH = True
 # Set the shadownet training batch size
-__C.TRAIN.BATCH_SIZE = 32
+__C.TRAIN.BATCH_SIZE = 256
 # Set the shadownet validation batch size
-__C.TRAIN.VAL_BATCH_SIZE = 32
+__C.TRAIN.VAL_BATCH_SIZE = 256
 # Set the learning rate decay steps
-__C.TRAIN.LR_DECAY_STEPS = 10000
+__C.TRAIN.LR_DECAY_STEPS = 30000
 # Set the learning rate decay rate
 __C.TRAIN.LR_DECAY_RATE = 0.1
 
@@ -56,8 +56,8 @@ __C.TRAIN.LR_DECAY_RATE = 0.1
 __C.TEST = edict()
 
 # Set the GPU resource used during testing process
-__C.TEST.GPU_MEMORY_FRACTION = 0.5
+__C.TEST.GPU_MEMORY_FRACTION = 0.8
 # Set the GPU allow growth parameter during tensorflow testing process
 __C.TEST.TF_ALLOW_GROWTH = False
 # Set the test batch size
-__C.TEST.BATCH_SIZE = 32
+__C.TEST.BATCH_SIZE = 128
